83263908ce435a390e02550aaf053708cfa21858
mifta00 <miftata00@gmail.com>
2025-06-10 12:01:05 +0700

    membuat login

---
+++ lib/auth/regristrasi_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegristasiPage extends StatefulWidget {
+  const RegristasiPage({super.key});
+
+  @override
+  State<RegristasiPage> createState() => _RegristasiPageState();
+}
+
+class _RegristasiPageState extends State<RegristasiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
